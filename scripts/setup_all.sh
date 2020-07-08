dfe37a1057d901752febd3f6940f578ecdad6d8f
Dafne van Kuppevelt <d.vankuppevelt@esciencecenter.nl>
2020-07-08 15:53:03 +0200

    Fixed setup script

--- scripts/setup_all.sh
+++ scripts/setup_all.sh
@@ -17,4 +17,5 @@ TF_CFLAGS=( $(python -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.ge
 TF_LFLAGS=( $(python -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.get_link_flags()))') )
 
 # Linux
+mkdir -p e2edutch/lib
 g++ -std=c++11 -shared e2edutch/coref_kernels.cc -o e2edutch/lib/coref_kernels.so -fPIC ${TF_CFLAGS[@]} ${TF_LFLAGS[@]} -O2 -D_GLIBCXX_USE_CXX11_ABI=0
