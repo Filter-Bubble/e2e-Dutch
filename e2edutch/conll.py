d2ddd18f3f8284243b05ba4799bb6c7a32034270
Dafne van Kuppevelt <d.vankuppevelt@esciencecenter.nl>
2020-09-18 10:47:16 +0200

    Removed obsolete line

--- e2edutch/conll.py
+++ e2edutch/conll.py
@@ -98,7 +98,6 @@ def output_conll_align(input_file, output_file, predictions):
             output_file.write(line)
             output_file.write("\n")
         else:
-            assert get_doc_key(row[0], row[1]) == doc_key
             coref_list = []
             if word_index in end_map:
                 for cluster_id in end_map[word_index]:
